e11456a269466e3ec4d502256dccba24abadc20b
Gisele <gisele_s@hotmail.com>
2023-04-24 21:07:45 -0300

    schema cadastro

--- src/schemas/user.schemas.js
+++ src/schemas/user.schemas.js
@@ -4,7 +4,7 @@ export const cadastroSchema = joi.object({
     nome: joi.string().required(),
     email: joi.string().email().required(),
     senha: joi.string().min(3).required(),
-    repeteSenha: joi.ref('senha')
+    repeteSenha: joi.valid(joi.ref('senha')).required()
 })
 
 export const loginSchema = joi.object({
